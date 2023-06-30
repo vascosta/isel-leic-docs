e5a0cf4eac5c06044172e90d8ab5c9c5bf77481b
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-06-30 15:45:22 +0100

    Update SafeContainer.kt

--- 4th Semester/PC/Exams/EN-2122V/SafeContainer.kt
+++ 4th Semester/PC/Exams/EN-2122V/SafeContainer.kt
@@ -24,24 +24,3 @@ class SafeContainer<T>(private val values: Array<SafeValue<T>>) {
         return null
     }
 }
-
-class UnsafeValue<T>(val value: T, initialLives: Int) {
-    val lives = AtomicInteger(initialLives)
-}
-class UnsafeContainer<T>(private val values: Array<UnsafeValue<T>>){
-    private val index = AtomicInteger(0)
-    fun consume(): T? {
-        while (true) {
-            val observedIndex = index.get()
-            if (observedIndex >= values.size) return null
-
-            val observedLives = values[observedIndex].lives.get()
-
-            if (observedLives > 0) {
-                if (values[observedIndex].lives.compareAndSet(observedLives, observedLives - 1))
-                    return values[observedIndex].value
-            }
-            index.compareAndSet(observedIndex, observedIndex + 1)
-        }
-    }
-}
